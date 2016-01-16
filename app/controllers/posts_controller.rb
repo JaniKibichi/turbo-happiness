ede37934a9de373f1e416965c4dcb37e5a9bb491
janikibichi <janikibichi@gmail.com>
2016-01-16 07:59:33 -0500

    better external url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
  before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
- before_action :authenticate_user!, except: [:index, :show]
+ before_action :authenticate_user!, except: [:index, :show, :search]
 
  def search
   if params[:search].present?
